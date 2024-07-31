0a91902129e595117906af2f9e5d623cd91f5c7d
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-07-31 16:22:23 -0300

    testes com border radius

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,31 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
 
-   
+   <div class="paia">
+       <div class="campo">
+           <h1>Nome</h1>
+       </div>
+       <img src="@/assets/pao.jpeg" alt="">
+    </div>
 </template>
 
 <style scoped>
+.paia{
+    width: 30vh;
+    height: 50vh;
+}
+img{
+    width: 30vh;
+    height: 50vh;
+}
 
+ 
+.campo {
+    height: 10vh;
+    top: 46px;
+    position: relative;
+    border-bottom-left-radius: 60px;
+    border-bottom-right-radius: 60px;
+    background-color: brown;
+}
 </style>
