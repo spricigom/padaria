1f7005712167f94f4a54529604df30bec3007e79
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-08-16 16:48:04 -0300

    fazendo pagina cadastro

--- src/views/CardapioView.vue
+++ src/views/CardapioView.vue
@@ -8,55 +8,101 @@ import HeaderNav from '@/componentes/HeaderNav.vue'
     </header>
 
     <body>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="@/assets/salgados.webp" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
+        <div class="fundo">
+            <div class="textoCima text-center">
+                <h1>AAAAAAAAAAAAA</h1>
+                <p>akpjaskaklnalkas</p>
+            </div>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="@/assets/salgados.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="@/assets/paoDeQueijo.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="@/assets/paoDeQueijo.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="@/assets/pizza.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="@/assets/pizza.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="@/assets/paoDeQueijo.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="@/assets/paoDeQueijo.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="@/assets/salgados.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="@/assets/paoDeQueijo.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="@/assets/pizza.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
+
     </body>
-
-
-
 </template>
 
 
 <style scoped>
-body{
+.fundo{
+    height: 100vh;
     background-color: #fcf9c6;
+    padding-bottom: 10px;
+}
+
+.textoCima{
+    top: 15vh;
 }
 </style>
